3464d3ecd0e8f781b1199dc4acb8eb9709c8de47
wajjfreelance <39485182+wajjfreelance@users.noreply.github.com>
2018-08-10 06:47:45 +0800

    sa

---
+++ storage/framework/views/a94caaf06700c32daac5f21b62170bd42608ee30.php
@@ -0,0 +1,39 @@
+<div class="booklist-container">
+    <div class="booklist-title">List of Books</div>
+    <div class="booklist-row">
+        <div class="perbook-container">
+            <div class="perbook-img"><img src="/img/Book1.jpg" alt=""></div>
+            <div class="perbook-title" data-toggle="modal" data-target="#myModal">Game of Thrones</div>
+        </div>
+        <div class="perbook-container">
+            <div class="perbook-img"><img src="/img/Book1.jpg" alt=""></div>
+            <div class="perbook-title" data-toggle="modal" data-target="#myModal">Book One Sample</div>
+        </div>
+    </div>
+</div>
+
+
+<!-- The Modal -->
+<div class="modal fade" id="myModal">
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content">
+      
+        <!-- Modal Header -->
+        <div class="modal-header">
+          <h4 class="modal-title">Modal Heading</h4>
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+        </div>
+        
+        <!-- Modal body -->
+        <div class="modal-body">
+          Modal body..
+        </div>
+        
+        <!-- Modal footer -->
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        </div>
+        
+      </div>
+    </div>
+  </div>
