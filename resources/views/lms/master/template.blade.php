e0451c95f9cee29135a92d0701b5eb229acf5c57
wajjfreelance <39485182+wajjfreelance@users.noreply.github.com>
2018-08-15 01:26:26 +0800

    modal

--- resources/views/lms/master/template.blade.php
+++ resources/views/lms/master/template.blade.php
@@ -19,13 +19,13 @@
 <!-- @yield('about') -->
 <!-- @yield('contact') -->
 @yield('footer')
-<script>
+
     <script src="assets/js/vendor/jquery-2.1.4.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js"></script>
     <script src="assets/js/plugins.js"></script>
     <script src="assets/js/main.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.bundle.min.js"></script>
-</script>
+
 </body>
 </html>
