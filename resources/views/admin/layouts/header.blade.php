ea0458ea881651bab6da3cbdc33161bb70e1e5e1
wajjfreelance <39485182+wajjfreelance@users.noreply.github.com>
2018-08-14 23:22:41 +0800

    sa

--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -28,7 +28,7 @@
                         <div class="user-menu dropdown-menu">
                                 <a class="nav-link" href="#"><i class="fa fa- user"></i>My Profile</a>
 
-                                <a class="nav-link" href="#"><i class="fa fa- user"></i>Notifications <span class="count">13</span></a>
+                               
 
                                 <a class="nav-link" href="#"><i class="fa fa -cog"></i>Settings</a>
 
